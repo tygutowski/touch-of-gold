7a57b957668b9f4c239cbc9f134bc1e723e0e1d7
tygutowski <tygutowski@gmail.com>
2023-03-23 13:00:00 -0400

    Added multiple tilemaps. Gold conversion working!

--- Midas.gd
+++ Midas.gd
@@ -13,8 +13,9 @@ func _physics_process(delta):
 	if tilemap.get_cell_atlas_coords(0, player_tile) == Vector2i(-1, -1):
 		var tiles_to_change = [Vector2i(player_tile.x+1, player_tile.y), Vector2i(player_tile.x-1, player_tile.y), Vector2i(player_tile.x, player_tile.y+1), Vector2i(player_tile.x, player_tile.y-1)]
 		for tile in tiles_to_change:
-			if tilemap.get_cell_atlas_coords(0, tile) != Vector2i(-1, -1):
-				tilemap.set_cell(0, tile, 0, Vector2i(2,0))
+			var tile_to_swap = tilemap.get_cell_atlas_coords(0, tile)
+			if tile_to_swap != Vector2i(-1, -1):
+				tilemap.set_cell(0, tile, 1, tile_to_swap)
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
