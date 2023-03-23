3b5620e5b32c860d0ab4adf1c2f4d20d7e43757b
tygutowski <tygutowski@gmail.com>
2023-03-23 12:07:57 -0400

    got the touch mechanic partially working

--- Gold.gd
+++ Gold.gd
@@ -1,14 +1,20 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@onready var tilemap = get_node("../TileMap")
+const SPEED = 90.0
+const JUMP_VELOCITY = -150.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var gravity = 500
 
 
 func _physics_process(delta):
+	var player_tile = get_global_transform().origin / 8
+	if tilemap.get_cell_atlas_coords(0, player_tile) == Vector2i(-1, -1):
+		var tiles_to_change = [Vector2i(player_tile.x+1, player_tile.y), Vector2i(player_tile.x-1, player_tile.y), Vector2i(player_tile.x, player_tile.y+1), Vector2i(player_tile.x, player_tile.y-1)]
+		for tile in tiles_to_change:
+			if tilemap.get_cell_atlas_coords(0, tile) != Vector2i(-1, -1):
+				tilemap.set_cell(0, tile, 0, Vector2i(2,0))
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
